188054a620c6235398579e3c8bec2abc18029ebf
alanlwang1 <alanwangsd@gmail.com>
2021-05-13 11:58:56 -0700

    update readme.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Lab7_Solution
+# CSE110 SP21 Lab 7
+
+## Author(s):
+- Alan Wang
+
+Published site: https://alanlwang1.github.io/Lab7/
--- sw.js
+++ sw.js
@@ -11,30 +11,30 @@ const CACHE_URL = ['https://cse110lab6.herokuapp.com/entries'];
 self.addEventListener('install', function(event) {
     // Perform install steps
     event.waitUntil(
-      caches.open(CACHE_NAME)
+        caches.open(CACHE_NAME)
         .then(function(cache) {
-          console.log('Opened cache');
-          return cache.addAll(CACHE_URL);
+            console.log('Opened cache');
+            return cache.addAll(CACHE_URL);
         })
     );
-  });
+});
 
-  // Activation event listener
-  self.addEventListener('activate', event => {
+// Activation event listener
+self.addEventListener('activate', event => {
     event.waitUntil(clients.claim());
-  });
+});
 
-  // Fetch event listener 
-  self.addEventListener('fetch', function(event) {
+// Fetch event listener 
+self.addEventListener('fetch', function(event) {
     event.respondWith(
-      caches.match(event.request)
+        caches.match(event.request)
         .then(function(response) {
-          // Cache hit - return response
-          if (response) {
+            // Cache hit - return response
+            if (response) {
             return response;
-          }
-          return fetch(event.request);
+            }
+            return fetch(event.request);
         }
-      )
+        )
     );
-  });
+});
